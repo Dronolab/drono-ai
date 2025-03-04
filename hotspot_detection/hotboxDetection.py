336360681ac43152ffc9f345bcd3f8b86bbc2c48
Felix Parent <68659805+pupil247@users.noreply.github.com>
2025-03-04 15:01:11 -0500

    Update hotboxDetection.py
    changed image from path to actual img frame

--- hotspot_detection/hotboxDetection.py
+++ hotspot_detection/hotboxDetection.py
@@ -7,7 +7,7 @@ This module provides hotspot detection using openCV with threshold control
 import cv2
 import matplotlib.pyplot as plt
 
-def get_img_hotspots(threshold, img_path, plot=True):
+def get_img_hotspots(threshold, frame, plot=True):
     """
     Compute the hotspot's bounding boxes and display it on the image
 
@@ -20,33 +20,33 @@ def get_img_hotspots(threshold, img_path, plot=True):
     An Array of Array of Int The coordinates and the size of the bounding boxes(x,y,height,width)
     """
 
-    image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    #convert to grayscale
+    if len(frame.shape) == 3:
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    #gaussian blur
+    blurred = cv2.GaussianBlur(frame_gray, (5, 5), 0)
 
     # Adjust the threshold value as needed
     _, threshold = cv2.threshold(blurred, threshold, 255, cv2.THRESH_BINARY)
 
+    #create contours
     contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Creating hotboxes
-    result = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    # Creating hotboxes and displaying result
     bounding_boxes = []
     for cnt in contours:
         if cv2.contourArea(cnt) > 50:  # Filter small regions
             x, y, w, h = cv2.boundingRect(cnt)
             bounding_boxes.append([x,y,w,h])
-            cv2.rectangle(result, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow("Thermal Hotspot Detection", frame)
     if plot:
-        plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+        plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         plt.title('Hotspot Detection')
         plt.show()
 
     return bounding_boxes
-
-
-#Testing the function
-hotspots = get_img_hotspots(200,'thermal-camera-crop.jpg',True)
-print(hotspots)
 
 
